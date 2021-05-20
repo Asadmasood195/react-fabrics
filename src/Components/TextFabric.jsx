551ff008d43bed3f80d08268b38484ffbf9962d6
usama-govsoft <robert@govsoft.us>
2021-05-20 15:44:06 +0500

    feat(Text Fabric): :sparkles: reuseable function, colors,font family change text

--- src/Components/TextFabric.jsx
+++ src/Components/TextFabric.jsx
@@ -1,100 +1,120 @@
+/* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react'
 import { fabric } from 'fabric'
 import '../App.css'
 
+var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E"
 
 const TextFabric = () => {
     const [canvas, setCanvas] = useState('')
     const [userText, setuserText] = useState('Hy Fabrics')
-    const [userText2, setuserText2] = useState('Hy Fabrics')
-    const [userText3, setuserText3] = useState('Hy Fabrics')
-    const [userText4, setuserText4] = useState('Hy Fabrics')
+
+
+
+    var img = document.createElement('img')
+    img.src = deleteIcon
 
     useEffect(() => {
         setCanvas(initCanvas())
+        fabric.Object.prototype.objectCaching = true
     }, [])
+
     const initCanvas = () => (
         new fabric.Canvas('canvas', {
-            height: 120,
-            width: 300,
+            // isDrawingMode: true,
+            centeredRotation: true,
+            controlsAboveOverlay: true,
+            selection: true,
+            renderOnAddRemove: true,
+            selectionColor: 'blue',
+            // freeDrawingBrush: new fabric.PencilBrush({ decimate: 8 }),
+            height: 150,
+            width: 400,
             backgroundColor: 'rgba( 212, 177, 177, 0.20)',
-
-
         })
     )
-    const addRect = canvi => {
-        setCanvas(initCanvas())
+
+
+    // style text function
+    const textStyle = (canvi, col, font, fontWeight, shadow, degree) => {
+        console.log(col)
         var text = new fabric.Text(userText, {
-            stroke: '#ff1318',
-            strokeWidth: 2
-        })
-        // console.log(canvi.onGesture())
-        canvi.add(text)
-    }
-
-    const Text2 = canvi => {
-        setCanvas(initCanvas())
-        var text = new fabric.Text(userText2, {
-            textAlign: 'right',
-            shadow: 'green -5px -5px 3px',
-        })
-        // text._fontSizeMult()
-        canvi.add(text)
-    }
-
-
-    const Text3 = canvi => {
-        setCanvas(initCanvas())
-        var text = new fabric.Text(userText3, {
-            fontStyle: 'italic',
-            fontFamily: 'https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@450',
-
-            stroke: 'orange',
-            strokeWidth: 2
-        })
-        text.animate('angle', 8, {
-            onChange: canvas.renderAll.bind(canvas),
-        })
-        text.animate('left', 50, {
-            onChange: canvas.renderAll.bind(canvas),
-            duration: 1000,
-            easing: fabric.util.ease.easeOutBounce
-        })
-        // text.set({ color: 'red' })
-        canvi.add(text)
-    }
-
-
-
-    const Text4 = canvi => {
-        setCanvas(initCanvas())
-        var text = new fabric.Text(userText4, {
+            fontFamily: font,
+            fontWeight: fontWeight,
             left: 100,
-            top: 10,
-            fill: '#f55',
-            angle: 25,
-            fontStyle: 'italic',
-            fontFamily: 'Delicious'
+            top: 50,
+            width: 200,
+            height: 100,
+            objectCaching: false,
+            stroke: col,
+            shadow: shadow ? 'green -5px -5px 3px' : '',
+            angle: degree ? degree : '',
+            hasRotatingPoint: true,
 
+            // scaleY: '3'
+            // fill: 'blue',
+            // strokeWidth: 4,
         })
-
-        // var text = new fabric.Color('rgb(100,100,100)').toHex()
-
         canvi.add(text)
-
     }
+
+
+
+    // delete activeObject function
+    const deleteObject = () => {
+        var activeObject = canvas.getActiveObject()
+        if (activeObject) {
+            canvas.remove(activeObject)
+        }
+    }
+
+    fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+        x: 0.5,
+        y: -0.5,
+        offsetY: 16,
+        cursorStyle: 'pointer',
+        mouseUpHandler: deleteObject,
+        render: renderIcon,
+        cornerSize: 24
+    })
+
+    // show delete button on active object function
+    function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+        var size = this.cornerSize
+        ctx.save()
+        ctx.translate(left, top)
+        ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle))
+        ctx.drawImage(img, -size / 2, -size / 2, size, size)
+        ctx.restore()
+    }
+
+
+
+    const changeText = (e) => {
+
+        if (e.target.value.length >= 1) {
+            setuserText(e.target.value)
+        } else {
+            setuserText('Hy Fabrics')
+
+        }
+    }
+
     return (
         <div>
             <aside className='asideBar'>
                 <div className='grid'>
-                    <button className='text1' onClick={() => addRect(canvas)}></button>
-                    <button className='text4' onClick={() => Text4(canvas)}></button>
-                    <button className='text2' onClick={() => Text2(canvas)}></button>
-                    <button className='text3' onClick={() => Text3(canvas)}></button>
+                    <button className='text1' onClick={() => textStyle(canvas, "red", 'Roboto Mono', 'normal', 'shadow', '-10')}></button>
+                    <button className='text2' onClick={() => textStyle(canvas, 'lightgreen', 'Poppins', 'italic', '', '30')}></button>
+                    <button className='text3' onClick={() => textStyle(canvas, 'blue', 'Pattaya', '900')}></button>
                 </div>
             </aside>
             <br /><br />
             <main >
+                <div>
+                    <input className='input' type="text" placeholder='Enter text here' onChange={changeText} />
+                </div>
+                <br />
                 <canvas id="canvas" />
             </main>
         </div>
